0ff3f73f3bff0a9f2131ec5fc6f542c5994a1c44
정영민 <metalbird0@gmail.com>
2019-07-15 19:02:47 +0900

    initialize commit

---
+++ src/main/java/com/metalbird/beautier/BeautierApplication.java
@@ -0,0 +1,13 @@
+package com.metalbird.beautier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautierApplication.class, args);
+	}
+
+}
